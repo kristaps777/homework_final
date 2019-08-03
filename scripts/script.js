d16387382511b189a7a9af642ad2638c6c87ca7b
Kristaps Vecvagars <vecvagars.kristaps@gmail.com>
2019-08-03 19:12:51 +0300

    added responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ function move() {
     const moveForm = document.getElementById('moveable');
     const loginForm = document.getElementById('login_form');
     const regForm = document.getElementById('register_form');
-    moveForm.style.left =('53%');
+    moveForm.style.left =('50%');
     regForm.setAttribute('class', 'inactive');
     loginForm.setAttribute('class', 'form_data');
 };
@@ -11,7 +11,25 @@ function moveBack() {
     const moveFormB = document.getElementById('moveable');
     const regForm = document.getElementById('register_form');
     const loginForm = document.getElementById('login_form');
-    moveFormB.style.left =('2%');
+    moveFormB.style.left =('0%');
+    regForm.setAttribute('class', 'form_data');
+    loginForm.setAttribute('class', 'inactive');
+};
+
+function moveMobile() {
+    const moveForm = document.getElementById('moveable_mobile');
+    const loginForm = document.getElementById('login_form_mobile');
+    const regForm = document.getElementById('register_form_mobile');
+    moveForm.style.top =('50%');
+    regForm.setAttribute('class', 'inactive');
+    loginForm.setAttribute('class', 'form_data');
+};
+
+function moveBackMobile() {
+    const moveFormB = document.getElementById('moveable_mobile');
+    const regForm = document.getElementById('register_form_mobile');
+    const loginForm = document.getElementById('login_form_mobile');
+    moveFormB.style.top =('0%');
     regForm.setAttribute('class', 'form_data');
     loginForm.setAttribute('class', 'inactive');
 };
